76dde26db849467581c028c21be571dd7199ca9c
Ivan Schuster <68964475+ChutiReality@users.noreply.github.com>
2023-03-15 13:24:09 -0300

    code logic changed

--- client_example.py
+++ client_example.py
@@ -1,15 +1,23 @@
+import time
 import asyncio
 import websockets
+import threading
 
-async def websocket_server(websocket, path):
+async def websocket_client(websocket, path):
     async for message in websocket:
         print(f"Received message: {message}")
         await websocket.send(message)
 
-async def start_server():
-    async with websockets.serve(websocket_server, "localhost", 8000) as server:
+async def server_handler():
+    async with websockets.serve(websocket_client, "localhost", 8000) as server:
         print(server)
         await asyncio.Future()
+        
+def run_server():
+    asyncio.run(server_handler())
+        
+ws_thread = threading.Thread(target = run_server)     
+ws_thread.start()       
 
-if __name__ == "__main__":
-    asyncio.run(start_server())
+while True:
+    time.sleep(1)
