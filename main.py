df671eaedfdc89288cc5fcd03f1572072db596e9
Ivan Schuster <68964475+ChutiReality@users.noreply.github.com>
2023-03-15 13:23:38 -0300

    can now open multiple sockets

--- main.py
+++ main.py
@@ -1,19 +1,29 @@
-from modules import utils
-import asyncio
 import json
+import time
+import asyncio
+import threading
 
+from modules import utils
 from modules import heartbeat
 
 with open("sockets.json", "r", encoding="utf8") as _sockets:
     sockets = json.load(_sockets)
 
-for service in sockets:
+async def gatherer():
+    tasks = []
+    for service in sockets:
+        hb_class = heartbeat.Heartbeat(
+            name = service["Name"], 
+            send_message = service["Message"], 
+            interval = service["Interval"], 
+            timeout = service["Timeout"]
+        )
+        tasks.append(asyncio.create_task(hb_class.start()))
+        
+    await asyncio.gather(*tasks)
 
-    hb = heartbeat.Heartbeat(
-        name = service["Name"],
-        send_message = service["Message"],
-        interval = service["Interval"],
-        timeout = service["Timeout"]
-    )
+async def main():
+    gatherer_task = asyncio.create_task(gatherer())
+    await gatherer_task
 
-asyncio.run(hb.start())
+asyncio.run(main())
